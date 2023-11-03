3cccc705daa27dc49810ba5999a8ae062c1e4d49
100yan-100yanov <stoyanps88@gmail.com>
2023-11-03 12:02:05 +0200

    Removed redundant classes, work done on entities

--- OnlineGameStore/playtray/src/main/java/com/playtray/model/entity/product/BaseProduct.java
+++ OnlineGameStore/playtray/src/main/java/com/playtray/model/entity/product/BaseProduct.java
@@ -1,11 +1,14 @@
 package com.playtray.model.entity.product;
 
 import com.playtray.model.entity.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.OneToMany;
+import com.playtray.model.enums.Category;
+import com.playtray.model.enums.Condition;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Size;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @MappedSuperclass
@@ -15,6 +18,19 @@ public class BaseProduct extends BaseEntity {
     @Size(min = 5, max = 200)
     private String description;
 
-    @OneToMany
-    List<Rating> ratings;
+    @Column(nullable = false)
+    private String image;
+
+    @Column(unique = true, nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Condition condition;
+
+    @Enumerated(EnumType.STRING)
+    private Category category; //TODO is it needed?
+
+    @Column(nullable = false)
+    @DecimalMin("5")
+    @DecimalMax("5000")
+    private BigDecimal price;
+
 }
---
+++ OnlineGameStore/playtray/src/main/java/com/playtray/model/enums/GenreName.java
@@ -0,0 +1,5 @@
+package com.playtray.model.enums;
+
+public enum GenreName {
+    ACTION, ADVENTURE, STRATEGY, RPG, HORROR, SPORT, RACING, FIGHTING
+}
